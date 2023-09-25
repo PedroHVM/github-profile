b024998f6059795250324f628e251db30924c423
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-09-25 19:08:44 -0300

    chore: project setup

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  blue: '#3294F8',
+  'base-title': '#E7EDF4',
+  'base-subtitle': '#C4D4E3',
+  'base-text': '#AFC2D4',
+  'base-span': '#7B96B2',
+  'base-label': '#3A536B',
+  'base-border': '#1C2F41',
+  'base-background': '#071422',
+  'base-input': '#040F1A',
+} as const
